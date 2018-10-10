82e0f2af7315824d13c54e11ddb7d2656efdeaeb
Aleksandra Suruło <aleksandrasurulo4@gmail.com>
2018-10-10 15:28:11 +0200

    fractions

--- fraction.py
+++ fraction.py
@@ -14,19 +14,80 @@ from math import gcd
 
 
 class Fraction(object):
-    """
-    Fraction class.
-    """
+    nom = None  # nominator
+    den = None  # denominator
 
-    def __init__(self, nom, denom):
-        self.nom = nom
-        self.denom = denom
+    def __init__(self, nom, den):
+        if den == 0:
+            raise ValueError("0 in denominator")
 
+        elif den < 0:
+            den = - den
+            nom = - nom
+
+            if nom == 0:
+                self.nom = int(nom)
+                self.den = int(den)
+
+            elif gcd(nom, den) > 1:
+                den1 = den // gcd(nom, den)
+                nom = nom // gcd(nom, den)
+                den = den1 // gcd(nom, den)
+
+        elif nom < 0 and den < 0:
+            nom = - nom
+            den = - den
+
+        elif nom == 0:
+            self.nom = int(nom)
+            self.den = int(den)
+
+        elif gcd(nom, den) > 1:
+            den1 = den // gcd(nom, den)
+            nom = nom // gcd(nom, den)
+            den = den1 // gcd(nom, den)
+
+        else:
+            den = den
+            nom = nom
+
+        self.nom = int(nom)
+        self.den = int(den)
+
+    # addition
+    def __add__(self, other):
+        if (self.nom * other.den + self.den * other.nom) == 0:
+            return 0
+        else:
+            return Fraction(self.nom * other.den + self.den * other.nom, self.den * other.den)
+
+    # multiplication
+    def __mul__(self, other):
+        if self.nom * other.nom == 0:
+            return 0
+        else:
+            return Fraction(self.nom * other.nom, self.den * other.den)
+
+    # division
+    def __truediv__(self, other):
+        if (self.nom * other.den) == 0:
+            return 0
+        else:
+            return Fraction(self.nom * other.den, self.den * other.nom)
+
+    # subtraction
+    def __sub__(self, other):
+        if (self.nom * other.den - self.den * other.nom) == 0:
+            return 0
+        else:
+            return Fraction(self.nom * other.den - self.den * other.nom, self.den * other.den)
+
+    # formating
     def __str__(self):
-        return f"{self.nom}/{self.denom}"
-
-    def __repr__(self):
-        return f"Fraction({self.nom}, {self.denom})"
+        return "{0.nom}/{0.den}".format(self)
 
     def __eq__(self, other):
-        return self.nom == other.nom and self.denom == other.denom
+        return self.nom == other.nom and self.den == other.den
+    def :
+        fracv = fracv.value()
+        return fracv
